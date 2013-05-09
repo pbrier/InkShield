c85599e9895b67c6d85938bbef70c4f3207a89dd
pbrier <git@pbrier.nl>
2013-05-09 23:27:14 +0200

    Now with G4 to wait for queue to empty: it works OK!

--- R2C2/gcode/maketest.lua
+++ R2C2/gcode/maketest.lua
@@ -6,20 +6,20 @@ print("; R2C2 Inkshield test file");
 print("G92 X0 Y0 E0 Z0; zero");
 print("G90; absolote positions");
 print("M200 E1000 ; 1000 pulses/mm");
-print("G1 F60    ; 1 mm/sec");
+print("G1 F600    ; 1 mm/sec");
 print("M701 S10  ; pulse duration 5usec");
 
 
 function send_wait(s)
   print(s);
-  print("M606;");
+  print("G4;");
 end;
 
 s = 1;
 x = 0;
-e = 0;
-
-send_wait("G1 X0 Y0");
+e = 2;
+send_wait("M700 S0");
+send_wait("G1 X0 Y0 E2");
 
 -- single nozzles
 for i=1,12 do
@@ -27,8 +27,8 @@ for i=1,12 do
   e = e + 1;
   send_wait("M700 S" .. s); -- select nozzle  
   send_wait("G1 X" .. x .. "E" .. e ); -- move
-  x = x + 0.5;
-  send_wait("G1 X" .. x ); -- move
+  --x = x + 0.5;
+  --send_wait("G1 X" .. x ); -- move
   s = s * 2;
 end
 
@@ -41,6 +41,6 @@ for i=1,12 do
   s = ((s+1) * 2)-1;
   send_wait("M700 S" .. s);
   send_wait("G1 X" .. x .. "E" .. e );
-  x = x + 0.5;
-  send_wait("G1 X" .. x ); -- move 
+ -- x = x + 0.5;
+ -- send_wait("G1 X" .. x ); -- move 
 end
